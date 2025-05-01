d6fa8c850e89fc1554792604ea721b5bfc6a8139
arlind <arlind.gurgurovci@gmail.com>
2025-05-01 20:00:11 +0200

    small fix, router added from login to dashboard

--- app/useLogin.ts
+++ app/useLogin.ts
@@ -1,13 +1,16 @@
 import { setCookie, getCookie } from "lib/apicalls"
+import { useRouter } from "next/navigation";
 
 export function useLogin() {
+  const route = useRouter();
 
   const login = async (key: string) => {
     const data = await setCookie("apiKey", key); // Setze den Cookie mit dem API-Key
     console.log(data);
 
-    const testLogs = await checkApiKey();
-    console.log('Res von route.ts',testLogs) // Teste den API-Key
+
+    const test = await checkApiKey();
+    if (test) route.push("/dashboard");
     return true;
   };
 
@@ -18,10 +21,10 @@ export function useLogin() {
   }
 
   const checkApiKey = async () => {
-    const response = await fetch("/api/torn/get-profile")
+    const response = await fetch("/api/torn/get-user-data?selection=basic")
     const data = await response.json();
-    return data
+    console.log(data);
+    if (data.error) return false; else return true;
   }
   return { login, getApiKey, checkApiKey };
-
 }
